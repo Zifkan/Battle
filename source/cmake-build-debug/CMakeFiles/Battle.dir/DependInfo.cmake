9c7901c768c569ed35fde4408b1531112e1c3085
Ylly <zifkanishe@gmail.com>
2020-05-05 19:35:40 +0300

    first commit

---
+++ source/cmake-build-debug/CMakeFiles/Battle.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Battle/source/AppEditorLogic.cpp" "F:/Battle/source/cmake-build-debug/CMakeFiles/Battle.dir/AppEditorLogic.cpp.obj"
+  "F:/Battle/source/AppSystemLogic.cpp" "F:/Battle/source/cmake-build-debug/CMakeFiles/Battle.dir/AppSystemLogic.cpp.obj"
+  "F:/Battle/source/AppWorldLogic.cpp" "F:/Battle/source/cmake-build-debug/CMakeFiles/Battle.dir/AppWorldLogic.cpp.obj"
+  "F:/Battle/source/ComponentSystem/ComponentSystem.cpp" "F:/Battle/source/cmake-build-debug/CMakeFiles/Battle.dir/ComponentSystem/ComponentSystem.cpp.obj"
+  "F:/Battle/source/main.cpp" "F:/Battle/source/cmake-build-debug/CMakeFiles/Battle.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ source/cmake-build-debug/CMakeFiles/Battle.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "Battle_x64d.lib"
+  "CMakeFiles/Battle.dir/AppEditorLogic.cpp.obj"
+  "CMakeFiles/Battle.dir/AppSystemLogic.cpp.obj"
+  "CMakeFiles/Battle.dir/AppWorldLogic.cpp.obj"
+  "CMakeFiles/Battle.dir/ComponentSystem/ComponentSystem.cpp.obj"
+  "CMakeFiles/Battle.dir/main.cpp.obj"
+  "F:/Battle/bin/Battle_x64d.exe"
+  "F:/Battle/bin/Battle_x64d.exe.manifest"
+  "F:/Battle/bin/Battle_x64d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Battle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
